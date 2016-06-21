30007f0a1efa57d6f570233512f04eee44a32569
Alexander Entinger <cto@lxrobotics.com>
2016-06-21 12:37:08 +0200

    Adding basic menu for manipulating a point

--- src/point_publisher/main.cpp
+++ src/point_publisher/main.cpp
@@ -14,6 +14,8 @@
  * PROTOTYPES
  **************************************************************************************/
 
+void handleExit        ();
+void handleInvalidValue();
 
 /**************************************************************************************
  * MAIN
@@ -25,12 +27,46 @@ int main(int argc, char **argv)
   
   ros::NodeHandle node_handle;
 
-  ros::spin();
-  
+  /* Create the publisher for publishing Point messages */
+
+  ros::Publisher point_publisher = node_handle.advertise<geometry_msgs::Point>("/point", 10);
+
+  /* Provide a crude menu for selecting which service one wants to invoke */
+
+  char cmd = 0;
+
+  do
+  {
+    std::cout << "Move" << std::endl;
+    std::cout << "  [u]p" << std::endl;
+    std::cout << "  [d]own" << std::endl;
+    std::cout << "  [l]eft" << std::endl;
+    std::cout << "  [r]righ" << std::endl;
+    std::cout << "[q]uit" << std::endl;
+
+    std::cout << ">>"; std::cin >> cmd;
+
+    switch(cmd)
+    {
+    case 'q': handleExit                 ();                       break;
+    default:  handleInvalidValue         ();                       break;
+    }
+  } while(cmd != 'q');
+
   return EXIT_SUCCESS;
 }
 
 /**************************************************************************************
  * OUR FUNCTIONS
  **************************************************************************************/
+
+void handleExit()
+{
+  std::cout << "Exiting point_publisher_node" << std::endl;
+}
+
+void handleInvalidValue()
+{
+  std::cout << "Invalid input value" << std::endl;
+}
 
